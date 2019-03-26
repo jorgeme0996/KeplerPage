ac048c0e9f3e42775bf44144262d048df34c5415
Alejandro Soto <alexsotog82@gmail.com>
2019-03-26 15:40:32 -0600

    Se sube la pagina de Kepler

---
+++ app.js
@@ -0,0 +1,17 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    nodemailer      = require("nodemailer");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.listen(3000, function(){
+    console.log("Esta vivo!!!!")
+})
+//process.env.PORT,process.env.IP
