38739a2a3d0e34ca3bc6e585535ca533bb02b2ab
Jorge Martinez <jorgeme0996@gmail.com>
2019-04-05 09:58:19 -0600

    Versión 1.0 deploy

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.post("/contacto", function(req, res) {
     res.render('ok');
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT,process.env.IP, function(){
     console.log("Esta vivo!!!!")
 })
 //process.env.PORT,process.env.IP
